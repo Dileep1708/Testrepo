0ada5c1d51a1f9d159b251171eba21d7f12ef88c
Dileep1708 <113967752+Dileep1708@users.noreply.github.com>
2025-02-12 15:24:24 +0530

    Add files via upload

---
+++ ProductSearchController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductSearch.Services;
+
+namespace ProductSearch.Controllers
+{
+    public class ProductSearchController : Controller
+    {
+        private readonly IProductSearchService _productSearchService;
+
+        public ProductSearchController(IProductSearchService productSearchService)
+        {
+            _productSearchService = productSearchService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string model, string name, string description, string manufacturer, string attributes)
+        {            var products = _productSearchService.GetProductDetails(model, name, description, manufacturer, attributes);
+            return View(products);
+        }
+    }
+}
+

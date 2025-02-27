089ec32cc746cf8c34c8def052184786ce1938df
Dileep1708 <113967752+Dileep1708@users.noreply.github.com>
2025-02-27 19:19:03 +0530

    Add files via upload

---
+++ TrackerUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using TrackerLibrary;
+
+namespace TrackerUI
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfiguration configuration = builder.Build();
+
+            GlobalConfig.IsDbConnectionEnabled = false; // Set this to false to disable the database connection
+            GlobalConfig.InitializeConnections(DatabaseType.TextFile, configuration);
+
+
+            Application.Run(new TournamentDashboardForm());
+        }
+    }
+}
